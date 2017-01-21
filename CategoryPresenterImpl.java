85b95795f2b6c2e27d48020ddcbc85e461d713cb
chengmingwei_1984122@126.com <chengmingwei_1984122@126.com>
2017-01-21 19:24:08 +0800

    爱美食

---
+++ CategoryPresenterImpl.java
@@ -0,0 +1,14 @@
+package cookbook.cmw.com.cookbook.category.dao.impl;
+
+import cookbook.cmw.com.cookbook.category.dao.inter.CategoryPresenter;
+
+/**
+ * Created by Administrator on 2017/1/21 0021.
+ */
+
+public class CategoryPresenterImpl implements CategoryPresenter {
+    @Override
+    public void getCategory() {
+        
+    }
+}
